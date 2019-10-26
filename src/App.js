0c1c89a7c5c4fb5b4a3469d5989439d42c9842c8
mestima <mestima@icloud.com>
2019-10-26 03:40:05 +0300

    state with update functionality

--- src/App.js
+++ src/App.js
@@ -3,13 +3,37 @@ import Settings from './settings';
 import './App.css';
 import HeaderImg from './components/headerimage/headerimage';
 import NavBar from './components/navbar/navbar';
+import Loader from './components/loader/loader';
 
 document.title = Settings.title
 export default class App extends React.Component {
+  state = {
+    page: '',
+    loading: true,
+    date: new Date(),
+
+    username: 'Анон',
+    email: '',
+    usergroup: 'none'
+  }
+
+  constructor() {
+    super();
+    setTimeout( () => {
+      this.setState({loading: false});
+    }, 1000);
+  }
+
+  updateState = (data) => {
+    this.setState(data);
+  }
+
   render() {
     return(<div>
-      <HeaderImg />
-      <NavBar />
+      {this.state.loading ? <Loader /> : <div>
+        <HeaderImg />
+        <NavBar username={this.state.username} updateState={this.updateState}/>
+      </div>}
     </div>);
   }
 }
