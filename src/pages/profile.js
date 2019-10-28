09dc6093ce73fe6c6366e71eda4285ff328edace
mestima <mestima@icloud.com>
2019-10-28 22:46:58 +0300

    profile page update

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -13,6 +13,19 @@ export default class IndexPage extends React.Component {
       </> : <>
         <HeaderImg />
         <NavBar token={this.props.token} username={this.props.username} authed={this.props.authed} updateState={this.props.updateState} usergroup={this.props.usergroup} needAdminBtn={needAdminBtn} needMainPBtn={needMainPBtn} />
+        <div className="container mx-auto justify-start pixel">
+          <br />
+          <div className="flex-auto text-gray-700 text-center bg-gray-400 px-4 py-2 m-2 rounded-full">
+              <p class="text-black">Имя: {this.props.username}</p>
+          </div>
+          <div className="flex-auto text-gray-700 text-center bg-gray-400 px-4 py-2 m-2 rounded-full">
+              <p class="text-black">Статус аккаунта: {this.props.usergroup}</p>
+          </div>
+          <div class="flex-auto text-gray-700 text-center text-xs bg-gray-400 px-4 py-2 m-2 rounded-full">
+            <p class="text-black">API Key:</p>
+            <p class="text-black">{this.props.token}</p>
+          </div>
+        </div>
       </>}
     </>)
   }
