dc945ab6bc2ab3cf63d2a94d7c4e72175bb58589
Mestima <mestima@icloud.com>
2019-11-26 22:52:01 +0300

    inventory page update

---
+++ src/components/invcontent/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class InvContent extends React.Component {
+  render() {
+    return <>
+      <br />
+      <div className="flex justify-center pixel">
+        <h1>Страница находится в разработке!</h1>
+      </div>
+      <div className="flex justify-center pixel">
+        <h3>Загляни позже, поняша :3</h3>
+      </div>
+    </>
+  }
+}
--- src/pages/inv.js
+++ src/pages/inv.js
@@ -3,6 +3,7 @@ import { Redirect } from 'react-router-dom';
 import Change from '../components/buttons/change';
 import NavBar from '../components/navbar/';
 import HeaderImg from '../components/headerimage/';
+import Content from '../components/invcontent/';
 
 const needMainPBtn = true;
 const needProfiBtn = true;
@@ -15,42 +16,7 @@ export default class IndexPage extends React.Component {
       </> : <>
         <HeaderImg />
         <NavBar token={this.props.token} username={this.props.username} authed={this.props.authed} updateState={this.props.updateState} usergroup={this.props.usergroup} needAdminBtn={needAdminBtn} needMainPBtn={needMainPBtn} needProfiBtn={needProfiBtn} />
-        <div className="container mx-auto justify-start">
-          <div className="flex-auto text-black text-center bg-gray-400 px-4 py-2 m-2 rounded-full pixel">
-            Статус Вашего аккаунта: {this.props.usergroup}
-          </div>
-          <br />
-          <div className="flex-auto text-black text-center px-4 py-2 m-2">
-            <form className="max-w-sm mx-auto">
-            <div className="md:flex md:items-center mb-6 pixel">
-              <div className="md:w-1/3">
-                <label className="block font-bold md:text-right mb-1 md:mb-0 pr-4">
-                  Новый Пароль
-                </label>
-              </div>
-              <div className="md:w-2/3">
-                <input className="bg-gray-600 appearance-none border-2 border-gray-200 text-black rounded-full w-full py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 pixel" id="password1" type="password" />
-              </div>
-            </div>
-              <div className="md:flex md:items-center mb-6 pixel">
-                <div className="md:w-1/3">
-                  <label className="block font-bold md:text-right mb-1 md:mb-0 pr-4">
-                    Повторите пароль
-                  </label>
-                </div>
-                <div className="md:w-2/3">
-                  <input className="bg-gray-600 appearance-none border-2 border-gray-200 text-black rounded-full w-full py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 pixel" id="password2" type="password" />
-                </div>
-              </div>
-              <div className="md:flex md:items-center">
-                <div className="md:w-1/3"></div>
-                <div className="md:w-2/3">
-                  <Change />
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
+        <Content />
       </>}
     </>)
   }
