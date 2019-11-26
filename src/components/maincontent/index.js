61010fecabe20785ce76b3be3a3aa505c2c7e481
Mestima <mestima@icloud.com>
2019-11-26 22:55:18 +0300

    Main page content update

--- src/components/maincontent/index.js
+++ src/components/maincontent/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class MainContent extends React.Component {
   render() {
     return <>
       <br />
-      <div className="flex justify-center">
-        <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-          Приходи на наш конвент!
-        </button>
-      </div>
+      <Link to="/convention">
+        <div className="flex justify-center">
+          <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            Приходи на наш конвент!
+          </button>
+        </div>
+      </Link>
       <div className="flex justify-center">
         А тут снова текст!
       </div>
