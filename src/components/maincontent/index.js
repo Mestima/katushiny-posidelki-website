ecdf00cab07600ace7fe933b6a1d916124dc457f
Mestima <mestima@icloud.com>
2019-11-26 22:29:56 +0300

    Main Content component updated

--- src/components/maincontent/index.js
+++ src/components/maincontent/index.js
@@ -3,7 +3,15 @@ import React from 'react';
 export default class MainContent extends React.Component {
   render() {
     return <>
-      <h1>Hello from MainContent to the World!</h1>
+      <br />
+      <div className="flex justify-center">
+        <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Приходи на наш конвент!
+        </button>
+      </div>
+      <div className="flex justify-center">
+        А тут снова текст!
+      </div>
     </>
   }
 }
