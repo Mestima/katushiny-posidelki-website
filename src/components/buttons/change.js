db98b640584636b5c0f5705570ad2fa2f865f14f
Mestima <mestima@icloud.com>
2019-11-29 02:19:28 +0300

    change btn update

--- src/components/buttons/change.js
+++ src/components/buttons/change.js
@@ -4,7 +4,7 @@ export default class Btn extends React.Component {
   render() {
     return(
       <div className="inline m-1">
-        <button className="bg-purple-500 hover:bg-purple-400 text-white font-bold px-4 border-b-4 border-purple-700 hover:border-purple-500 rounded" onClick={this.props.change}>
+        <button className="bg-purple-500 hover:bg-purple-400 text-white font-bold px-4 border-b-4 border-purple-700 hover:border-purple-500 rounded">
           Изменить
         </button>
       </div>
