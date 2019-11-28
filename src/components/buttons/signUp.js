9ffac18f36deddbb79d4a9b781398f66b3e32108
Mestima <mestima@icloud.com>
2019-11-29 02:20:05 +0300

    signup btn update

--- src/components/buttons/signUp.js
+++ src/components/buttons/signUp.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Btn extends React.Component {
   render() {
     return(
       <div className="inline m-1">
-        <button className="bg-green-500 hover:bg-green-400 text-white font-bold px-4 border-b-4 border-green-700 hover:border-green-500 rounded" onClick={this.props.signUp}>
-          Регистрация
+        <button className="bg-green-500 hover:bg-green-400 text-white font-bold px-4 border-b-4 border-green-700 hover:border-green-500 rounded">
+          <Link to="/reg">Регистрация</Link>
         </button>
       </div>
     )
