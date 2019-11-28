08ae60cc1de32ca561987ae90f29887e38b81f3a
Mestima <mestima@icloud.com>
2019-11-29 01:44:05 +0300

    Convention page content logic update

--- src/components/concontent/index.js
+++ src/components/concontent/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+import Donate from '../donate/';
 import Buy from '../buttons/buy';
 import IsJson from '../isJson/';
 import LocalLoader from '../loader/loader';
@@ -44,13 +45,34 @@ export default class ConContent extends React.Component {
 
   createTable = () => {
     let table = [];
-    for (let v of this.state.result) {
-      table.push(
-        <tr><td class='border px-4 py-2'>{v['name']}</td>
-        <td class='border px-4 py-2'>{v['description']}</td>
-        <td class='border px-4 py-2'>{v['cost']}</td>
-        <td class='border px-4 py-2'><Buy link={v['link']} /></td></tr>
-      );
+    let flag = false;
+    for (let k of Object.keys(this.state.result)) {
+      if (flag == true) {
+        let v = this.state.result[k];
+        table.push(
+          <tr><td class='border px-4 py-2'>{v['name']}</td>
+          <td class='border px-4 py-2'>{v['description']}</td>
+          <td class='border px-4 py-2'>{v['cost']}</td>
+          <td class='border px-4 py-2'><Buy link={v['link']} /></td></tr>
+        );
+      }
+      flag = true;
+    }
+    return table;
+  }
+
+  createInfo = () => {
+    let table = [];
+    for (let k of Object.keys(this.state.result)) {
+      let v = this.state.result[k];
+      table.push(<div class='text-center'>
+        <div class='text-5xl'>{v['name']}</div>
+        <div class='text-3xl'>{v['description']}</div>
+        <div class='text-base'>Дата начала мероприятия: {v['starts']}</div>
+        <div class='text-base'>Дата окончания мероприятия: {v['ends']}</div>
+        <div class='pixel'>Уже куплено билетов: {v['visitors']}</div>
+      </div>);
+      break;
     }
     return table;
   }
@@ -58,10 +80,13 @@ export default class ConContent extends React.Component {
   render() {
     return <>
       {this.state.loading ? <LocalLoader /> : <>
-      <div className="flex justify-center">
-        Hello World!
+      <div className="flex justify-center mt-4 mb-4">
+        {this.createInfo()}
       </div>
-      <div className="flex justify-center container">
+      <div className="flex justify-center text-4xl font-bold">
+        Билеты
+      </div>
+      <div className="flex justify-center container mb-4">
         <table className="table-auto">
           <thead>
             <tr>
@@ -76,8 +101,14 @@ export default class ConContent extends React.Component {
           </tbody>
         </table>
       </div>
+      <div className="flex justify-center">
+        Если Вы не можете прийти на мероприятие, Вы можете пожертвовать любую удобную Вам сумму.
+      </div>
+      <div className="flex justify-center">
+        Все, кто пожертвовал деньги, получат благодарность со сцены, а также на сайте и в группе мероприятия!
+      </div>
       <div className="flex justify-center mb-20">
-        Hello World!
+        <Donate />
       </div> </> }
     </>
   }
