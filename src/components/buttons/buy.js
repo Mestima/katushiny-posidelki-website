a964089201cc76301c753e2a386da75cf28d568d
Mestima <mestima@icloud.com>
2019-11-29 00:14:30 +0300

    buy button logic update

--- src/components/buttons/buy.js
+++ src/components/buttons/buy.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 export default class Btn extends React.Component {
+  buy = (link) => {
+    window.open(
+      link,
+      '_blank'
+    );
+  }
+
   render() {
     return(
       <div className="inline m-1">
-        <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={this.props.buy}>
+        <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={() => this.buy(this.props.link)}>
           Купить
         </button>
       </div>
