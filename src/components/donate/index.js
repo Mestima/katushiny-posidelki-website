3a27961635f57f9e932f746f127cacc3bba461bb
Mestima <mestima@icloud.com>
2019-11-29 01:43:39 +0300

    donate iframe created

---
+++ src/components/donate/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import IFrame from 'react-iframe';
+
+export default function() {
+    return(<>
+      <IFrame url="https://auth.robokassa.ru/Merchant/PaymentForm/FormFLS.if?MerchantLogin=katusha_events&InvoiceID=0&Culture=ru&Encoding=utf-8&SignatureValue=2acc909ad4ee087c2a3a8d773f247d9f%22/"
+              width="450px"
+              height="70px"
+              id="myId"
+              className="myClassname"
+              display="initial"
+              position="relative"/>
+    </>)
+}
