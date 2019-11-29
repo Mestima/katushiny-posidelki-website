94aa15ad11df9189526b8912a9d515ace9584cb7
Mestima <mestima@icloud.com>
2019-11-29 21:40:56 +0300

    api component update

--- src/components/api/index.js
+++ src/components/api/index.js
@@ -1,7 +1,13 @@
-let dname = 'http://site.ru';
+let protocol = 'http://';
+let name = 'nilored.ru';
+let inventory = 'inv.';
+
+let dname = protocol+name;
+let aname = protocol+inventory+name;
 export default {
   login: dname+'/login.php',
   reg: dname+'/reg.php',
   convention: dname+'/convention.php',
-  mainpage: dname+'/mainpage.php'
+  mainpage: dname+'/mainpage.php',
+  inventory: aname+'?token='
 }
