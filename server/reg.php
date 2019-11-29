d92ba72255195ec8c3b570c4ec408c65829d7b87
Mestima <mestima@icloud.com>
2019-11-29 16:59:32 +0300

    server reg.php update

--- server/reg.php
+++ server/reg.php
@@ -2,7 +2,7 @@
   header("Access-Control-Allow-Origin: *");
   header("Access-Control-Allow-Methods: GET, POST, PUT");
   header("Access-Control-Allow-Headers: Content-Type");
-    header("Access-Control-Allow-Credentials: true");
+  header("Access-Control-Allow-Credentials: true");
 
   require_once("./config.php");
 
