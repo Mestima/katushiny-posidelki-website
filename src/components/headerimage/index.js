155667ffba9c2b0947900f746cc9fc58f278838d
Mestima <mestima@icloud.com>
2019-12-03 22:35:31 +0300

    headerimage container-fluid -> container

--- src/components/headerimage/index.js
+++ src/components/headerimage/index.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import Settings from '../../settings';
 
 export default function HeaderImage() {
-  return(<div className="container-fluid">
+  return(<div className="container">
       <Link to="/"><img className="img-fluid center-block" src={Settings.headerimg} /></Link>
     </div>);
 }
