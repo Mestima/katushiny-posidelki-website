6900ea872d0522d5be0e0391dae212a1926faa35
Mestima <mestima@icloud.com>
2019-12-03 22:52:11 +0300

    warning component created

---
+++ src/components/admin_attention_warn/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function() {
+  return(<>
+    <div className="container">
+      <h6>
+        Организаторы оставляют за собой право изменять список товаров,
+        билетов и цен на все виды услуг и продукции.
+      </h6>
+    </div>
+  </>)
+}
